9e0375ac226741f4bb9035e5431f4f6f96361125
Santhosh M <santhoshm309@gmail.com>
2020-09-11 13:28:17 +0530

    Collapsing TAB layout - Done

--- app/src/main/java/com/example/plauction/Activities/MainActivity.java
+++ app/src/main/java/com/example/plauction/Activities/MainActivity.java
@@ -1,26 +1,27 @@
 package com.example.plauction.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.viewpager.widget.ViewPager;
 
-import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.view.WindowManager;
 import android.widget.RelativeLayout;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 
-import com.android.volley.VolleyError;
-import com.example.plauction.Adapters.TeamListSpinnerAdapter;
-import com.example.plauction.Common.CommonFunctions;
-import com.example.plauction.Entities.AuctionTeamsEntity;
+import com.example.plauction.Adapters.PageAdapter;
+import com.example.plauction.Common.CenteringTabLayout;
+import com.example.plauction.Fragments.TeamsFragment;
+import com.example.plauction.Fragments.LeaderboardFragment;
 import com.example.plauction.R;
-import com.example.plauction.RestClientImpl.RESTClientImplementation;
 import com.facebook.shimmer.ShimmerFrameLayout;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
 
 
 import java.util.ArrayList;
@@ -29,79 +30,84 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private ShimmerFrameLayout shimmerFrameLayout;
     private RelativeLayout relativeLayout;
-    CoordinatorLayout coordinatorLayout;
+    private CoordinatorLayout coordinatorLayout;
+    private PageAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    TextView pageTitle;
+    private AppBarLayout appBarLayout;
 
     private ArrayList<String> teamsList=new ArrayList<>();
-    private void loadData(){
-        shimmerFrameLayout.startShimmer();
-        shimmerFrameLayout.setVisibility(View.VISIBLE);
-
-        RESTClientImplementation.getAuctionTeams(new AuctionTeamsEntity.OnListLoad() {
-            @Override
-            public void onListLoaded(int code, final AuctionTeamsEntity[] auctionTeamsEntities, VolleyError volleyError) {
-                teamsList.add("SELECT A TEAM");
-                if(code == 200 && volleyError!=null){
-                    // Fetch teams
-                    for(int i=0; i<auctionTeamsEntities.length; i++)
-                    {
-                        teamsList.add(auctionTeamsEntities[i].getTeam_name().toUpperCase());
-                    }
-                    shimmerFrameLayout.stopShimmer();
-                    shimmerFrameLayout.setVisibility(View.GONE);
-                    relativeLayout.setVisibility(View.VISIBLE);
-                    CommonFunctions.makeSnackBar("Teams Data Fetched",relativeLayout).show();
-                }else if(code == 700){
-                    shimmerFrameLayout.stopShimmer();
-                    shimmerFrameLayout.setVisibility(View.GONE);
-                    CommonFunctions.makeToast("No internet connectivity",getApplicationContext());
-                }else {
-                    shimmerFrameLayout.stopShimmer();
-                    shimmerFrameLayout.setVisibility(View.GONE);
-                    CommonFunctions.makeToast(CommonFunctions.getErrorMessage(code, getApplicationContext()),getApplicationContext());
-                }
-            }
-        }, getApplicationContext());
-    }
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
         setContentView(R.layout.activity_main);
-        coordinatorLayout=findViewById(R.id.main_coordinate);
+        coordinatorLayout = findViewById(R.id.main_coordinate);
         shimmerFrameLayout = findViewById(R.id.shimmer_main);
-        relativeLayout = findViewById(R.id.relative_main);
+
+        appBarLayout = (AppBarLayout) findViewById(R.id.htab_appbar);
         shimmerFrameLayout.startShimmer();
-        loadData();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        // Get reference of widgets from XML layout
-        final Spinner spinner = (Spinner) findViewById(R.id.spinnerTeamName);
+        tabLayout =  findViewById(R.id.tab_layout);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
+        viewPager = (ViewPager) findViewById(R.id.pager);
 
-        // Initializing an ArrayAdapter
+        adapter = new PageAdapter(getSupportFragmentManager());
 
-        TeamListSpinnerAdapter teamListSpinnerAdapter = new TeamListSpinnerAdapter(this,R.layout.team_spinner_item,teamsList, spinner);
+        // Add 2 Fragments
 
-        teamListSpinnerAdapter.setDropDownViewResource(R.layout.team_spinner_item);
-        spinner.setAdapter(teamListSpinnerAdapter);
-        int getDefaultPos = teamListSpinnerAdapter.getPosition("SELECT A TEAM");
-        spinner.setSelection(getDefaultPos); // Default Message
+        // Teams Fragment
+        TeamsFragment teamsFragment = new TeamsFragment();
+        adapter.addFragment(teamsFragment, "TEAMS", 0);
+        tabLayout.addTab(tabLayout.newTab().setText(adapter.getPageTitle(0)));
 
-        // Listener for Spinner item
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        // Leader board Fragment
+        LeaderboardFragment leaderboardFragment = new LeaderboardFragment();
+        adapter.addFragment(leaderboardFragment, "LEADERBOARD", 1);
+        tabLayout.addTab(tabLayout.newTab().setText(adapter.getPageTitle(1)));
+        // viewpager
+
+        viewPager.setAdapter(adapter);
+
+        pageTitle = (TextView) findViewById(R.id.page_title);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedTeamName = (String) parent.getItemAtPosition(position);
-                if (position == 0)
-                    CommonFunctions.makeToast("PLEASE SELECT A TEAM",getApplicationContext());
-                else {
-                    // SELECTION LOGIC MAKE POST VOLLEY
-                }
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
 
             }
         });
+
+        viewPager.addOnPageChangeListener(new CenteringTabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+    }
+
+    public void onAdapterLoaded() {
+        Log.e("ON adapter","LOADED");
+        shimmerFrameLayout.stopShimmer();
+        shimmerFrameLayout.setVisibility(View.GONE);
+        coordinatorLayout.setVisibility(View.VISIBLE);
     }
 }
