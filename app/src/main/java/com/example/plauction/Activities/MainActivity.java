c5b02b3971c81a001c7674fcb7a45ff6a3606c0e
Santhosh M <santhoshm309@gmail.com>
2020-09-13 15:28:38 +0530

    Changed loadData to activity

--- app/src/main/java/com/example/plauction/Activities/MainActivity.java
+++ app/src/main/java/com/example/plauction/Activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.plauction.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Build;
@@ -10,21 +11,39 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
+import com.android.volley.VolleyError;
 import com.example.plauction.Adapters.PageAdapter;
+import com.example.plauction.Adapters.PlayersAdapter;
+import com.example.plauction.Adapters.TeamListSpinnerAdapter;
 import com.example.plauction.Common.CenteringTabLayout;
+import com.example.plauction.Common.CommonFunctions;
+import com.example.plauction.Entities.AuctionTeamsEntity;
+import com.example.plauction.Entities.BootstrapEntity;
+import com.example.plauction.Entities.Elements;
+import com.example.plauction.Entities.Playerinfo;
 import com.example.plauction.Fragments.TeamsFragment;
 import com.example.plauction.Fragments.LeaderboardFragment;
 import com.example.plauction.R;
+import com.example.plauction.RestClientImpl.RESTClientImplementation;
 import com.facebook.shimmer.ShimmerFrameLayout;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.tabs.TabLayout;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 
+import java.io.Serializable;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,46 +55,32 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     TextView pageTitle;
     private AppBarLayout appBarLayout;
-
+    Gson gson = new Gson();
     private ArrayList<String> teamsList=new ArrayList<>();
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // clear FLAG_TRANSLUCENT_STATUS flag:
+    ArrayList<Elements> elements;
+    private Bundle bundle = new Bundle();
+    private boolean isPagersSet=false;
 
-        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }
+    private void loadViewPager()
+    {
+        if(isPagersSet)
+            return;
 
-        setContentView(R.layout.activity_main);
-        coordinatorLayout = findViewById(R.id.main_coordinate);
-        shimmerFrameLayout = findViewById(R.id.shimmer_main);
-
-        appBarLayout = (AppBarLayout) findViewById(R.id.htab_appbar);
-        shimmerFrameLayout.startShimmer();
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        tabLayout =  findViewById(R.id.tab_layout);
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        viewPager = (ViewPager) findViewById(R.id.pager);
-
+        isPagersSet = true;
         adapter = new PageAdapter(getSupportFragmentManager());
 
         // Add 2 Fragments
 
         // Teams Fragment
         TeamsFragment teamsFragment = new TeamsFragment();
+        teamsFragment.setArguments(bundle);
         adapter.addFragment(teamsFragment, "TEAMS", 0);
         tabLayout.addTab(tabLayout.newTab().setText(adapter.getPageTitle(0)));
 
         // Leader board Fragment
         LeaderboardFragment leaderboardFragment = new LeaderboardFragment();
         adapter.addFragment(leaderboardFragment, "LEADERBOARD", 1);
+        leaderboardFragment.setArguments(bundle);
         tabLayout.addTab(tabLayout.newTab().setText(adapter.getPageTitle(1)));
         // viewpager
 
@@ -101,8 +106,100 @@ public class MainActivity extends AppCompatActivity {
         });
 
         viewPager.addOnPageChangeListener(new CenteringTabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+
+    private void loadData(){
+        shimmerFrameLayout.startShimmer();
+        shimmerFrameLayout.setVisibility(View.VISIBLE);
+        RESTClientImplementation.getBootstrapStatic(new BootstrapEntity.OnListLoad() {
+            @Override
+            public void onListLoaded(int code, BootstrapEntity bootstrapEntity, VolleyError volleyError) {
+                if(code == 200 && volleyError!=null){
+                    elements=bootstrapEntity.getElements();
+                    Map<Integer,Elements> map = new HashMap<Integer,Elements>();
+                    for (Elements e : elements){
+                        map.put(e.getId(),e);
+                    }
+                    CommonFunctions.setPlayerIdToElementMap_(map);
+                    // Set element in bundle
+                    // Convert elements to string
+                    Type listType = new TypeToken<List<Elements>>() {}.getType();
+                    String elementsJson=gson.toJson(elements,listType);
+                    bundle.putString("ELEMENTS", elementsJson);
+                    // This is a synchronous call as we need both data for our app
+                    RESTClientImplementation.getAuctionTeams(new AuctionTeamsEntity.OnListLoad() {
+                        @Override
+                        public void onListLoaded(int code, final AuctionTeamsEntity[] auctionTeamsEntities, VolleyError volleyError) {
+                            teamsList.add("SELECT A TEAM");
+                            if(code == 200 && volleyError!=null){
+                                // Set auction team players in bundle
+                                Type listType = new TypeToken<List<AuctionTeamsEntity>>() {}.getType();
+                                String teamsJson=gson.toJson(Arrays.asList(auctionTeamsEntities),listType);
+                                bundle.putString("AUCTION_TEAMS",teamsJson);
+                                loadViewPager();
+                                CommonFunctions.makeSnackBar("Teams Data Fetched",coordinatorLayout).show();
+
+                            }else if(code == 700){
+                                shimmerFrameLayout.stopShimmer();
+                                shimmerFrameLayout.setVisibility(View.GONE);
+                                CommonFunctions.makeToast("No internet connectivity",getApplicationContext());
+                            }else {
+                                shimmerFrameLayout.stopShimmer();
+                                shimmerFrameLayout.setVisibility(View.GONE);
+                                CommonFunctions.makeToast(CommonFunctions.getErrorMessage(code, getApplicationContext()),getApplicationContext());
+                            }
+                        }
+                    }, getApplicationContext());
+                }
+                else if(code == 700){
+                    shimmerFrameLayout.stopShimmer();
+                    shimmerFrameLayout.setVisibility(View.GONE);
+                    CommonFunctions.makeToast("No internet connectivity",getApplicationContext());
+                }
+                else
+                {
+                    shimmerFrameLayout.stopShimmer();
+                    shimmerFrameLayout.setVisibility(View.GONE);
+                    CommonFunctions.makeToast(CommonFunctions.getErrorMessage(code, getApplicationContext()),getApplicationContext());
+
+                }
+            }
+        },getApplicationContext());
+
 
     }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        setContentView(R.layout.activity_main);
+        coordinatorLayout = findViewById(R.id.main_coordinate);
+        shimmerFrameLayout = findViewById(R.id.shimmer_main);
+
+        appBarLayout = (AppBarLayout) findViewById(R.id.htab_appbar);
+        shimmerFrameLayout.startShimmer();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tabLayout =  findViewById(R.id.tab_layout);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        loadData();
+
+    }
+
 
     public void onAdapterLoaded() {
         Log.e("ON adapter","LOADED");
--- app/src/main/java/com/example/plauction/Fragments/TeamsFragment.java
+++ app/src/main/java/com/example/plauction/Fragments/TeamsFragment.java
@@ -31,10 +31,14 @@ import com.example.plauction.Entities.Playerinfo;
 import com.example.plauction.R;
 import com.example.plauction.RestClientImpl.RESTClientImplementation;
 import com.facebook.shimmer.ShimmerFrameLayout;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.security.Key;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TeamsFragment extends Fragment {
@@ -51,91 +55,55 @@ public class TeamsFragment extends Fragment {
     private ArrayList<ArrayList<Playerinfo>> teamPlayers=new ArrayList<>();
     private ArrayList<Elements> elements;
     private PlayersAdapter playersAdapter;
+    private ArrayList<AuctionTeamsEntity> auctionTeamsEntitiesList;
+    private  AuctionTeamsEntity[] auctionTeamsEntities;
+    private Gson gson=new Gson();
     private void loadData(){
-        shimmerFrameLayout.startShimmer();
-        shimmerFrameLayout.setVisibility(View.VISIBLE);
-        RESTClientImplementation.getBootstrapStatic(new BootstrapEntity.OnListLoad() {
-            @Override
-            public void onListLoaded(int code, BootstrapEntity bootstrapEntity, VolleyError volleyError) {
-                if(code == 200 && volleyError!=null){
-                    elements=bootstrapEntity.getElements();
-                }
-                else{
-                    shimmerFrameLayout.stopShimmer();
-                    shimmerFrameLayout.setVisibility(View.GONE);
-                    CommonFunctions.makeToast("No internet connectivity",context);
-                }
+        teamsList.add("SELECT A TEAM");
+        teamPlayers.add(null);
+
+        for(int i=0; i<auctionTeamsEntities.length; i++)
+        {
+            teamsList.add(auctionTeamsEntities[i].getTeamName().toUpperCase());
+            teamPlayers.add(auctionTeamsEntities[i].getPlayerInfo());
         }
-        },context);
 
-        RESTClientImplementation.getAuctionTeams(new AuctionTeamsEntity.OnListLoad() {
+        // Initializing an ArrayAdapter
+        TeamListSpinnerAdapter teamListSpinnerAdapter = new TeamListSpinnerAdapter(context,R.layout.team_spinner_item,teamsList, spinner);
+        teamListSpinnerAdapter.setDropDownViewResource(R.layout.team_spinner_item);
+        spinner.setAdapter(teamListSpinnerAdapter);
+        int getDefaultPos = teamListSpinnerAdapter.getPosition("SELECT A TEAM");
+        spinner.setSelection(getDefaultPos); // Default Message
+
+        // Listener for Spinner item
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onListLoaded(int code, final AuctionTeamsEntity[] auctionTeamsEntities, VolleyError volleyError) {
-                teamsList.add("SELECT A TEAM");
-                teamPlayers.add(null);
-                if(code == 200 && volleyError!=null){
-                    // Fetch teams
-                    for(int i=0; i<auctionTeamsEntities.length; i++)
-                    {
-                        teamsList.add(auctionTeamsEntities[i].getTeamName().toUpperCase());
-                        teamPlayers.add(auctionTeamsEntities[i].getPlayerInfo());
-                    }
-                    shimmerFrameLayout.stopShimmer();
-                    shimmerFrameLayout.setVisibility(View.GONE);
-                    relativeLayout.setVisibility(View.VISIBLE);
-                    // Initializing an ArrayAdapter
-                    TeamListSpinnerAdapter teamListSpinnerAdapter = new TeamListSpinnerAdapter(context,R.layout.team_spinner_item,teamsList, spinner);
-                    teamListSpinnerAdapter.setDropDownViewResource(R.layout.team_spinner_item);
-                    spinner.setAdapter(teamListSpinnerAdapter);
-                    int getDefaultPos = teamListSpinnerAdapter.getPosition("SELECT A TEAM");
-                    spinner.setSelection(getDefaultPos); // Default Message
-
-                    // Listener for Spinner item
-                    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                        @Override
-                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                            String selectedTeamName = (String) parent.getItemAtPosition(position);
-                            ArrayList<Playerinfo> selectedTeamPlayers= teamPlayers.get(position);
-                            if (position == 0) {
-                                CommonFunctions.makeToast("PLEASE SELECT A TEAM", context);
-                            }
-                            else {
-                                // SELECTION LOGIC MAKE POST VOLLEY
-                                Log.i("selected",selectedTeamName);
-                                //Log.i("players",selectedTeamPlayers.get(1).getPlayerId().toString());
-                                //Log.i("elements",elements.get(1).getTeam().toString());
-                                Map<Integer,Elements> map = new HashMap<Integer,Elements>();
-                                for (Elements e : elements){
-                                    map.put(e.getId(),e);
-                                }
-
-                                CommonFunctions.setPlayerIdToElementMap_(map);
-                                Log.i("SUM", CommonFunctions.getTeamTotalSum(selectedTeamPlayers)+"");
-                                playersNestedScrollView.setVisibility(View.VISIBLE);
-                                playersAdapter = new PlayersAdapter(getActivity(),selectedTeamPlayers,map);
-                                playerRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-                                playerRecyclerView.setAdapter(playersAdapter);
-                            }
-                        }
-
-                        @Override
-                        public void onNothingSelected(AdapterView<?> parent) {
-
-                        }
-                    });
-
-                    CommonFunctions.makeSnackBar("Teams Data Fetched",relativeLayout).show();
-                }else if(code == 700){
-                    shimmerFrameLayout.stopShimmer();
-                    shimmerFrameLayout.setVisibility(View.GONE);
-                    CommonFunctions.makeToast("No internet connectivity",context);
-                }else {
-                    shimmerFrameLayout.stopShimmer();
-                    shimmerFrameLayout.setVisibility(View.GONE);
-                    CommonFunctions.makeToast(CommonFunctions.getErrorMessage(code, context),context);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedTeamName = (String) parent.getItemAtPosition(position);
+                ArrayList<Playerinfo> selectedTeamPlayers= teamPlayers.get(position);
+                if (position == 0) {
+                    CommonFunctions.makeToast("PLEASE SELECT A TEAM", context);
+                }
+                else {
+                    // SELECTION LOGIC MAKE POST VOLLEY
+                    Log.i("SUM", CommonFunctions.getTeamTotalSum(selectedTeamPlayers)+"");
+                    playersNestedScrollView.setVisibility(View.VISIBLE);
+                    playersAdapter = new PlayersAdapter(getActivity(),selectedTeamPlayers,CommonFunctions.getPlayerIdToElementMap_());
+                    playerRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                    playerRecyclerView.setAdapter(playersAdapter);
                 }
             }
-        }, context);
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        shimmerFrameLayout.stopShimmer();
+        shimmerFrameLayout.setVisibility(View.GONE);
+        relativeLayout.setVisibility(View.VISIBLE);
+
+
     }
 
     @Override
@@ -178,8 +146,17 @@ public class TeamsFragment extends Fragment {
         playerRecyclerView=(RecyclerView) inflated_frag.findViewById(R.id.playersRecyclerView);
         playersNestedScrollView=(NestedScrollView)inflated_frag.findViewById(R.id.playersNestedScrollView);
         // load Data
-        loadData();
+        //loadData();
+        String elementsString=getArguments().getString("ELEMENTS");
+        Type listType = new TypeToken<List<Elements>>() {}.getType();
+        elements=gson.fromJson(elementsString, listType);
 
+        String auctionTeamsString=getArguments().getString("AUCTION_TEAMS");
+        listType = new TypeToken<List<AuctionTeamsEntity>>() {}.getType();
+        auctionTeamsEntitiesList=gson.fromJson(auctionTeamsString,listType);
+        auctionTeamsEntities = new AuctionTeamsEntity[auctionTeamsEntitiesList.size()];
+        auctionTeamsEntities=  auctionTeamsEntitiesList.toArray(auctionTeamsEntities);
+        loadData();
 
         ((MainActivity)activity).onAdapterLoaded();
         return inflated_frag;
